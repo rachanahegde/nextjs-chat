12eed9d1b66c27b6e67d90ae721f8753bcba4f67
rachanahegde <hegde.rachana99@gmail.com>
2025-07-23 17:00:25 +0100

    Update tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: [require("daisyui")],
+}
